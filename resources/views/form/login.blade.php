895e99d6e898f85718f97300a9cc369a16eeac60
khairil azizee <khairil114@squarework.net>
2021-03-30 17:28:42 +0800

    update

---
+++ resources/views/form/login.blade.php
@@ -0,0 +1,13 @@
+@extends('welcome')
+
+@section('mainbody')
+    
+    <div class="container mx-auto px-5 py-24">
+        <div class="flex flex-wrap align-middle">
+            <div class="lg:w-2/3 mx-auto">
+                
+            </div>
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('web.home');
 });
+
+Route::get('/login', [LoginController::class, 'index']);
 
