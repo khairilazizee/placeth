31bd5c6819d1a853b4c87f7231f7ed3285e86014
khairil azizee <khairil114@squarework.net>
2021-03-30 17:50:47 +0800

    update login form

--- resources/views/form/login.blade.php
+++ resources/views/form/login.blade.php
@@ -2,17 +2,33 @@
 
 @section('mainbody')
     
-    <div class="container mx-auto px-5 py-24">
-        <div class="flex flex-wrap align-middle">
+    <div class="container mx-auto px-5 py-24 h-screen">
+        <div class="flex flex-col-h align-middle">
             <div class="w-4/12 mx-auto px-6 py-5 bg-white rounded-lg shadow-lg">
-                <h2 class="font-bold text-gray-800">Login Form</h2>
+                <h2 class="font-bold text-center text-3xl text-gray-800">Login</h2>
                 <form action="" method="post" class="mt-10">
                     @csrf
-                    <label for="">Username</label>
-                    <input type="text">
+                    <label for="" class="block mt-10 font-semibold uppercase text-xs">E-mail</label>
+                    <input type="text" class="block w-full border-b-2 py-3 px-1 mt-4 appearance-none border-gray-100" placeholder="E-mail address">
 
-                    <label for="">Password</label>
-                    <input type="password">
+                    <label for="" class="block mt-10 font-semibold uppercase text-xs">Password</label>
+                    <input type="password" class="block w-full border-b-2 py-3 px-1 mt-4 appearance-none border-gray-100" placeholder="Password">
+
+                    <button class="w-full mt-10 py-3 bg-blue-500 text-white hover:bg-blue-700">Log In</button>
+
+                    <div class="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center">
+                        <a href="forgot-password" class="flex-2 underline">
+                            Forgot password?
+                        </a>
+    
+                        <p class="flex-1 text-gray-500 text-md mx-4 my-1 sm:my-auto">
+                            or
+                        </p>
+            
+                        <a href="register" class="flex-2 underline">
+                            Create an Account
+                        </a>
+                    </div>
                 </form>
             </div>
         </div>
