51c3f5134639f7f166b2eda12d9bb641115e025a
khairil azizee <khairil114@squarework.net>
2021-04-09 21:32:12 +0800

    update : dashboard page, add button in dashboard page, insert awesomefont

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,8 +4,41 @@
 
 @include('admin.topbar')
 
-<div class="container h-full">
-    
+<div class="w-full h-screen bg-gray-50">
+<section class="text-gray-600">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="grid grid-cols-4 -m-4 gap-1">
+            <a href="">
+                <div class="p-5 w-full bg-white shadow-md">
+                    <i class="fas fa-plus float-right align-middle"></i>
+                    <h2 class="text-3xl font-semibold text-black">2.7k</h2>
+                    <p>Businesses</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="p-5 w-full bg-white shadow-md">
+                    <i class="fas fa-eye float-right align-middle"></i>
+                    <h2 class="text-3xl font-semibold text-black">2.7k</h2>
+                    <p>Products</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="p-5 w-full bg-white shadow-md">
+                    <i class="fas fa-plus float-right align-middle"></i>
+                    <h2 class="text-3xl font-semibold text-black">2.7k</h2>
+                    <p>Users</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="p-5 w-full bg-white shadow-md">
+                    <i class="fas fa-eye float-right align-middle"></i>
+                    <h2 class="text-3xl font-semibold text-black">2.7k</h2>
+                    <p>Reviews</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
 </div>
 
 @endsection
